ed23e34cd67abf78273fce6df776fc4893828c1b
Kaif-Sasoli <kaifsasoli1@gmail.com>
2025-01-09 02:52:51 +0500

    Email

--- backend/app.js
+++ backend/app.js
@@ -1,48 +1,4 @@
 // //****** Requiring Packeges ********/
-// const express = require('express');
-// const cors = require('cors');
-// const cookieParser = require('cookie-parser');
-// const path = require('path');
-// const app = express();
-// const userRouter = require('./routes/user.router')
-// const imageModel = require('./models/image.model')
-// const { connectDB } = require('./config/dbConnection');
-
-// // Cors configuration
-// app.use(cors({
-//     origin: "http://localhost:5173",
-//     methods: "GET, POST, PATCH, DELETE",
-//     credentials: true
-// }))
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(cookieParser())
-// app.use(express.static(path.join(__dirname, 'public')));
-// require('dotenv').config();
-// connectDB(); // Connect to database
-
-
-
-// app.get('/image', (req, res) => {
-    
-    
-//     imageModel.create({
-//         imageUrl: "kaif",
-//         public_Id: "Sasdoihi",
-//         userId: "677dbcb59f48b8e6ac4764db"
-//     })
-
-//     res.status(200);
-// })
-
-// // Routes
-// app.use('/user', userRouter)
-
-// // Export the app
-// module.exports = app;
-
-
-//****** Requiring Packages ********/
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
@@ -54,25 +10,69 @@ const { connectDB } = require('./config/dbConnection');
 
 // Cors configuration
 app.use(cors({
-  origin: 'http://localhost:5173',  // React app URL (make sure it's correct)
-  methods: 'GET, POST, PATCH, DELETE',  // Allowed methods
-  credentials: true  // Allow cookies to be sent along with requests
-}));
-
+    origin: "http://localhost:5173",
+    methods: "GET, POST, PATCH, DELETE",
+    credentials: true
+}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
+app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')));
 require('dotenv').config();
+connectDB(); // Connect to database
 
-connectDB(); // Connect to the database
 
+
+app.get('/image', (req, res) => {
+    
+    
+    imageModel.create({
+        imageUrl: "kaif",
+        public_Id: "Sasdoihi",
+        userId: "677dbcb59f48b8e6ac4764db"
+    })
+
+    res.status(200);
+})
 
 // Routes
-app.use('/user', userRouter);
-
-// Handling Preflight (OPTIONS) requests (for CORS)
-app.options('*', cors()); // Ensure the server handles the preflight requests
+app.use('/user', userRouter)
 
 // Export the app
 module.exports = app;
+
+
+// //****** Requiring Packages ********/
+// const express = require('express');
+// const cors = require('cors');
+// const cookieParser = require('cookie-parser');
+// const path = require('path');
+// const app = express();
+// const userRouter = require('./routes/user.router')
+// const imageModel = require('./models/image.model')
+// const { connectDB } = require('./config/dbConnection');
+
+// // Cors configuration
+// app.use(cors({
+//   origin: 'http://localhost:5173',  // React app URL (make sure it's correct)
+//   methods: 'GET, POST, PATCH, DELETE',  // Allowed methods
+//   credentials: true  // Allow cookies to be sent along with requests
+// }));
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+// require('dotenv').config();
+
+// connectDB(); // Connect to the database
+
+
+// // Routes
+// app.use('/user', userRouter);
+
+// // Handling Preflight (OPTIONS) requests (for CORS)
+// app.options('*', cors()); // Ensure the server handles the preflight requests
+
+// // Export the app
+// module.exports = app;
