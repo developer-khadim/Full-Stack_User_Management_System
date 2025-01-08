26af842487ace1183d592235bf1e2ef5ef58892b
Kaif-Sasoli <kaifsasoli1@gmail.com>
2025-01-09 02:47:39 +0500

    Conflict

--- backend/app.js
+++ backend/app.js
@@ -64,32 +64,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 require('dotenv').config();
-<<<<<<< HEAD
-connectDB(); // Connect to database
 
-app.get('/otp', (req, res) => {
-    try {
-   res.status(200).send({ OTP : generateOTP()})
-    } catch(err){
-        res.status(500).send({error : error.message})
-    }
-})
-=======
 connectDB(); // Connect to the database
 
-// Sample image route (adjust as per requirement)
-app.get('/image', (req, res) => {
-  imageModel.create({
-    imageUrl: "kaif",
-    public_Id: "Sasdoihi",
-    userId: "677dbcb59f48b8e6ac4764db"
-  }).then(() => {
-    res.status(200).send('Image Created');
-  }).catch(err => {
-    res.status(500).send('Error creating image');
-  });
-});
->>>>>>> 847c45bd9047c2c45d01c16efa6176c0788c2b2b
 
 // Routes
 app.use('/user', userRouter);
